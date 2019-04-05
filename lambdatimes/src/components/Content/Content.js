48f9ded30f49b37b1d07506c57671b3a6e892661
Sean Doyle <sdoylelambda@gmail.com>
2019-04-05 16:37:52 -0400

    changeSelected function.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -24,7 +24,8 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-
+    const copiedComments = this.state.copiedComments
+    const arrayWithNewComment = copiedComments.push(commentText)
     
     // this.state.tabs.add(tab)
   };
