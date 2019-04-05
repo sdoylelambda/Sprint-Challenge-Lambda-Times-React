85d51feeab986c2fcee785e20bf2d289aa7055a1
Sean Doyle <sdoylelambda@gmail.com>
2019-04-05 18:25:33 -0400

    Tab.js code uncommented.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,43 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
  const Tab = props => {
-//   if (this.props.tab === selectedTab) {
-//     return className='tab active-tab';
-//   } else {
-//     return className='tab';
-//   }
-//   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-//       if they match, the className should be: 'tab active-tab', 
-//       if it is not it should just be 'tab'*/
-//   return (
-//     <div
-//       className={'tab'}
-//       onClick={() => {
-//         /* Replace this dummy click handler function with your 
-//         selectTabHandler function from props 
-//          you'll need to pass the `tab` in as an argument 
-//          to this handler. */
+  if (this.props.tab === selectedTab) {
+    return className='tab active-tab';
+  } else {
+    return className='tab';
+  }
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+      if they match, the className should be: 'tab active-tab', 
+      if it is not it should just be 'tab'*/
+  return (
+    <div
+      className={'tab'}
+      onClick={() => {
+        /* Replace this dummy click handler function with your 
+        selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument 
+         to this handler. */
 
-//          this.props.selectedTabHandler(this.props.tab)
+         this.props.selectedTabHandler(this.props.tab)
 
-//       }}
-//     >
-//       {props.tab.toUpperCase()}
-//     </div>
-//   );
+      }}
+    >
+      {props.tab.toUpperCase()}
+    </div>
+  );
  };
 
 // Make sure you include PropTypes on your props.
 
-// Tab.propTypes = {
-//   tab: PropTypes.arrayOf(
-//       PropTypes.shape({
-//           headLine: PropTypes.string,
-//           tab: PropTypes.string,
-//           img: PropTypes.string,
-//           author: PropTypes.string
-//            })
-//       )
-//   }
+Tab.propTypes = {
+  tab: PropTypes.arrayOf(
+      PropTypes.shape({
+          headLine: PropTypes.string,
+          tab: PropTypes.string,
+          img: PropTypes.string,
+          author: PropTypes.string
+           })
+      )
+  }
 
 export default Tab;
