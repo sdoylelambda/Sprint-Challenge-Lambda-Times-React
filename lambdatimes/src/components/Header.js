3ee61a5dc7213f594d6616041a6a5e2967160422
Sean Doyle <sdoylelambda@gmail.com>
2019-04-12 13:53:19 -0400

    Styled components added.

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  }
+  @media (min-width: 1280px) {
+  .header {
+    width: 1280px;
+  }
+`
+
 const Header = () => {
   return (
-    <div className="header">
+   <div> 
+     <HeaderStyle>
       <span className="date">SMARCH 32, 2018</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
